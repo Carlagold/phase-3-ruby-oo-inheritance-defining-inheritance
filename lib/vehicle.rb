063365abc487f365175e0d5e8cf91b88e399ce92
Carla <csachett@student.scf.edu>
2022-10-08 15:10:46 -0400

    Completed assignement

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,19 @@
+# Class Vehicle is the Super Class or Parent Class.
 class Vehicle
+    # giving attributes
+    attr_accessor :wheel_size, :wheel_number
+
+    def initialize(wheel_size, wheel_number)
+      @wheel_size = wheel_size
+      @wheel_number = wheel_number
+    end
+# The following two instance methods #go and #fill_up_tank describe some common vehicle behavior.
+def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
 
 end
